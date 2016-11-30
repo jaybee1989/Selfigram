8b574e6c6d045f978f9b5fbf668dd2306886e1a6
jaybee1989 <300169522@Administrators-Mac-mini-9.local>
2016-11-29 20:01:35 -0800

    completed assignments for week 8 day 1

---
+++ Selfigram/SelfieCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SelfieCellTableViewCell.swift
+//  Selfigram
+//
+//  Created by John Belcher on 2016-11-29.
+//  Copyright © 2016 Liz Demer. All rights reserved.
+//
+
+import UIKit
+
+class SelfieCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
